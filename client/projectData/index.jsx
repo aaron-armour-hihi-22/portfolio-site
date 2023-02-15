fe76d0a920f68b5b70c042d8a46a2833c33b764a
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-02-15 20:13:36 +1300

    Create a sample object of data for the projects

---
+++ client/projectData/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const projects = {
+  1: {
+    name: 'First project',
+    content: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dicta,
+          aspernatur nesciunt fugit fuga consequatur eos repellat nostrum
+          mollitia unde explicabo pariatur vitae quis, laborum sint doloremque
+          eveniet officiis saepe.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vel dicta
+          eligendi sint officiis. Modi soluta, obcaecati minus eos ratione
+          libero eveniet quibusdam alias suscipit hic nisi asperiores id. Iusto.
+        </p>
+      </>
+    ),
+  },
+  2: {
+    name: 'Second project',
+    content: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dicta,
+          aspernatur nesciunt fugit fuga consequatur eos repellat nostrum
+          mollitia unde explicabo pariatur vitae quis, laborum sint doloremque
+          eveniet officiis saepe.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vel dicta
+          eligendi sint officiis. Modi soluta, obcaecati minus eos ratione
+          libero eveniet quibusdam alias suscipit hic nisi asperiores id. Iusto.
+        </p>
+      </>
+    ),
+  },
+  123: {
+    name: 'Another project',
+    content: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dicta,
+          aspernatur nesciunt fugit fuga consequatur eos repellat nostrum
+          mollitia unde explicabo pariatur vitae quis, laborum sint doloremque
+          eveniet officiis saepe.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vel dicta
+          eligendi sint officiis. Modi soluta, obcaecati minus eos ratione
+          libero eveniet quibusdam alias suscipit hic nisi asperiores id. Iusto.
+        </p>
+      </>
+    ),
+  },
+}
+
+export default projects
