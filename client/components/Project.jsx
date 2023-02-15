7d11b342e34f7cd2852866b3f7987b0e9699240f
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-02-15 20:14:59 +1300

    Use the data from the object to populate the Project component

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import projects from '../projectData'
 
 function Project() {
   const { id } = useParams()
+  const projData = projects[id]
 
   return (
     <>
-      <h2>This is project {id}</h2>
+      <h2>{projData.name}</h2>
+      {projData.content}
       <Link to="/">Home</Link>
     </>
   )
