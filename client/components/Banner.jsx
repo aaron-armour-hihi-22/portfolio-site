50bfd68bd794d554db155ae799116ef177e19e60
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-02-15 15:34:27 +1300

    Add a Banner component with title and menu button

---
+++ client/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h2>Aaron Armour Dev Portfolio</h2>
+      <button>menu</button>
+    </div>
+  )
+}
+
+export default Banner
