cfa15aa621028b97d81687e2ff71b93986c80fc4
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-02-24 15:21:47 +1300

    Update the Menu with the new sections and links

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { HashLink as Link } from 'react-router-hash-link'
 import Box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
@@ -7,12 +7,14 @@ import ListItemText from '@mui/material/ListItemText'
 
 function Menu({ setDrawerIsOpen }) {
   const menuItems = [
-    ['Home', '20px', '/'],
-    ['Skills', '20px'],
-    ['Projects', '20px'],
+    ['Home', '20px', '/#'],
+    ['About Me', '20px', '/#about'],
+    ['Skills', '20px', '/#skills'],
+    ['Projects', '20px', '/#projects'],
     ['First Project', '40px', 'project/1'],
     ['Second Project', '40px', 'project/2'],
     ['Another Project', '40px', 'project/123'],
+    ['Contact', '20px', '/#contact'],
   ]
 
   return (
