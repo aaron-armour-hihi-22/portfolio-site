7172c9e3827fc488bd1be28ebb13aa73c4a2a338
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-02-27 20:20:43 +1300

    Add a Mui theme and use in the App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material'
 import Banner from './Banner'
 import Home from './Home'
 import Project from './Project'
 
+const theme = createTheme({
+  palette: {
+    contrastThreshold: 4.5,
+    background: {
+      paper: '#0E636E',
+    },
+  },
+  typography: {
+    allVariants: {
+      color: 'whitesmoke',
+    },
+    fontFamily: 'Gruppo, Verdana',
+  },
+})
+
 function App() {
   return (
     <>
-      <header className="header">
-        <Banner />
-      </header>
-      <section className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="project/:id" element={<Project />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </section>
+      <ThemeProvider theme={theme}>
+        <header className="header">
+          <Banner />
+        </header>
+        <section className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="project/:id" element={<Project />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </section>
+      </ThemeProvider>
     </>
   )
 }
