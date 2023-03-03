2e77c5f70b032eeaf598d49e02927ea5aa280eba
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-03-03 19:19:29 +1300

    Make the project image enlarge slightly when mouse hovers over the ProjectCard

--- client/components/ProjectCard.jsx
+++ client/components/ProjectCard.jsx
@@ -9,35 +9,37 @@ import Launch from '@mui/icons-material/Launch'
 
 function ProjectCard({ projectId, projectData }) {
   return (
-    <Card sx={{ width: '320px', padding: '5px', marginBottom: '10px' }}>
-      <CardActionArea component={Link} to={'project/' + projectId}>
-        <CardHeader title={projectData.name} />
-        <CardMedia
-          component="img"
-          image={projectData.image}
-          alt={projectData.altText}
-        />
-        <CardContent>
-          <Typography variant="body1">{projectData.summary}</Typography>
-        </CardContent>
-      </CardActionArea>
-      <div className="link-logos">
-        {projectData.deployed && (
-          <a href={projectData.deployed} target="_blank" rel="noreferrer">
-            <Launch fontSize="large" sx={{ color: 'white' }} />
-          </a>
-        )}
-        {projectData.github && (
-          <a href={projectData.github} target="_blank" rel="noreferrer">
-            <img
-              src="/assets/github-mark-white.png"
-              alt="github invertocat logo"
-              className="github-logo"
-            />
-          </a>
-        )}
-      </div>
-    </Card>
+    <div className="project-card">
+      <Card sx={{ width: '320px', padding: '5px', marginBottom: '10px' }}>
+        <CardActionArea component={Link} to={'project/' + projectId}>
+          <CardHeader title={projectData.name} />
+          <CardMedia
+            component="img"
+            image={projectData.image}
+            alt={projectData.altText}
+          />
+          <CardContent>
+            <Typography variant="body1">{projectData.summary}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <div className="link-logos">
+          {projectData.deployed && (
+            <a href={projectData.deployed} target="_blank" rel="noreferrer">
+              <Launch fontSize="large" sx={{ color: 'white' }} />
+            </a>
+          )}
+          {projectData.github && (
+            <a href={projectData.github} target="_blank" rel="noreferrer">
+              <img
+                src="/assets/github-mark-white.png"
+                alt="github invertocat logo"
+                className="github-logo"
+              />
+            </a>
+          )}
+        </div>
+      </Card>
+    </div>
   )
 }
 
